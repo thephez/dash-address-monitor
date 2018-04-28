40e157caebcb611f3fc847f229a856dbf1594acb
thephez <thephez@users.noreply.github.com>
2018-04-27 20:44:45 -0400

    Insight-API requests  - Add retries  - Check for numeric response

--- dash-address-monitor/dash-address-monitor.py
+++ dash-address-monitor/dash-address-monitor.py
@@ -2,7 +2,7 @@ from coin_rpc_class import RPCHost
 from keybase import Keybase
 
 from pprint import pprint
-import requests
+import time, requests
 
 #from blockcypher import get_address_details
 #from blockcypher import get_address_full
@@ -49,14 +49,46 @@ def getBalanceInsight(address):
     apiUrlSuffix = "/balance"
     url = '{}{}{}'.format(apiUrlBase, address, apiUrlSuffix)
 
-    response = requests.get(url)
+    tries = 5
+    hadConnectionFailures = False
+    while True:
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            tries -= 1
+            if tries == 0:
+                raise Exception('Failed to connect to Insight API at {} for balance check.'.format(apiUrlBase))
+            hadFailedConnections = True
+            print("Couldn't connect for balance check, will sleep for five seconds and then try again ({} more tries)".format(tries))
+            time.sleep(5)
+        else:
+            if (response.status_code == 400):
+                tries -= 1
+                print('Insight-Api request failure: {} {}. Retrying {} more times...'.format(response.status_code, response.reason, tries))
+                time.sleep(1)
+                continue
 
-    if response.status_code == requests.codes.ok:
-        print('{} {}'.format(response.status_code, response.text)) #, contents.json()))
-        return response.text
-    else:
-        response.raise_for_status()
-        #throw 'Invalid response: {}'.format(response.status_code)
+            if hadConnectionFailures:
+                print('Connected after retry.')
+
+            break
+
+    if not response.status_code in (200, 500):
+        raise Exception('Insight-API connection failure: ' + str(response.status_code) + ' ' + response.reason)
+
+    if (response.text.isdigit() != True):
+        raise Exception('Insight-API: Non-numeric balance returned')
+
+    print('Completed with {} tries remaining'.format(tries))
+
+    #if response.status_code == requests.codes.ok:
+    #    print('{} {}'.format(response.status_code, response.text)) #, contents.json()))
+    #    return response.text
+    #else:
+    #    response.raise_for_status()
+    #    #throw 'Invalid response: {}'.format(response.status_code)
+
+    return response.text
 
 def getBalanceBlockcypher(address):
 
